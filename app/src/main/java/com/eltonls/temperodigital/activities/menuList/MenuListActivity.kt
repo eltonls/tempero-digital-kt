df8fa72d1abfabf648e3fa54fed0942c2a1bdaeb
Elton <eltonrs1999@outlook.com>
2024-03-08 14:47:48 -0300

    Feat: Added Main Toolbar Layout

---
+++ app/src/main/java/com/eltonls/temperodigital/activities/menuList/MenuListActivity.kt
@@ -0,0 +1,12 @@
+package com.eltonls.temperodigital.activities.menuList
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.eltonls.temperodigital.R
+
+class MenuListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu_list)
+    }
+}
